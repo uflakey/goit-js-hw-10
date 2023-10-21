96a4ac78fe4cf10c70641683ceebdd319ef0795f
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-21 21:46:46 +0000

    Deploying to gh-pages from @ uflakey/goit-js-hw-10@ac398f05bd06deffe0a15171ce702a51995ad305 🚀

---
+++ index.a9586088.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".breed-select"),o=(document.querySelector(".loader"),document.querySelector(".error"),document.querySelector(".cat-info"));e.addEventListener("click",(function(){console.log(o)}))}();
+//# sourceMappingURL=index.a9586088.js.map
