6eaf8cbc11e34260826bfa6920ae89924fec3607
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-26 16:04:49 +0300

    1111

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,18 @@
-export function fetchBreeds() {}
+import axios from 'axios';
 
-export function fetchCatByBreed(breedId) {}
+axios.defaults.headers.common['x-api-key'] =
+  'live_MZDGaLKnSKgLejbGPt65KGPysAd1fongewuJTLPtyrW6kxT9KWZVLcByatwOYRCq';
+
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
+
+export function fetchBreeds() {
+  return axios.get('/breeds').then(respons => {
+    return respons.data;
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios.get(`/images/search?breed_ids=${breedId}`).then(resp => {
+    return resp.data;
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-// import SlimSelect from 'slim-select';
-// import ‘slim - select / dist / slimselect.css’;
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 
-// new SlimSelect({
-//   select: '#selectElement',
-// });
-
-import fetchBreeds from './cat-api';
-import fetchCatByBreed from './cat-api';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 const select = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
-const box = document.querySelector('.cat-info');
+const catInfo = document.querySelector('.cat-info');
 
-select.addEventListener('click', onClick);
-function onClick() {
-  console.log(box);
-}
+console.log(fetchBreeds);
+console.log(fetchCatByBreed);
+console.log(respons.data);
