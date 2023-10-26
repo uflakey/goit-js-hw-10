7fef6c1d2c77f55d3e9ab55c46cd4e512e3b6967
uflakey <134938478+uflakey@users.noreply.github.com>
2023-10-26 13:05:34 +0000

    Deploying to gh-pages from @ uflakey/goit-js-hw-10@6eaf8cbc11e34260826bfa6920ae89924fec3607 🚀

--- index.3746e481.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".breed-select"),o=(document.querySelector(".loader"),document.querySelector(".error"),document.querySelector(".cat-info"));e.addEventListener("click",(function(){console.log(o)}));
-//# sourceMappingURL=index.3746e481.js.map
--- index.a9586088.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".breed-select"),o=(document.querySelector(".loader"),document.querySelector(".error"),document.querySelector(".cat-info"));e.addEventListener("click",(function(){console.log(o)}))}();
-//# sourceMappingURL=index.a9586088.js.map
